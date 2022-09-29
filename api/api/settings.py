4a2c8d0ffb20c0463120583db079467bef3798f3
Abdulsamet Haymana <65792330+SametHaymana@users.noreply.github.com>
2022-09-29 23:38:50 +0300

    Update settings.py

--- api/api/settings.py
+++ api/api/settings.py
@@ -139,8 +139,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "peynirkarpuzekmek@gmail.com"
-EMAIL_HOST_PASSWORD = "mywpbrmmxqeoqvzc"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
 
 
 APPEND_SLASH= False
